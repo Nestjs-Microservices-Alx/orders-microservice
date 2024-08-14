00ce357ad5c0df3cf580293678fce7ad7447b860
Adrian <alextravc.342@gmail.com>
2024-08-13 20:16:42 -0500

    ADD: orders microservice initial setup

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,37 @@
+import {
+  Controller,
+  NotImplementedException,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderDto } from './dto/update-order.dto';
+import { OrdersService } from './orders.service';
+
+@Controller()
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @MessagePattern('createOrder')
+  create(@Payload() createOrderDto: CreateOrderDto) {
+    return this.ordersService.create(createOrderDto);
+  }
+
+  @MessagePattern('findAllOrders')
+  findAll() {
+    return this.ordersService.findAll();
+  }
+
+  @MessagePattern('findOneOrder')
+  findOne(@Payload('id', ParseIntPipe) id: number) {
+    return this.ordersService.findOne(id);
+  }
+
+  @MessagePattern('changeOrderStatus')
+  changeOrderStatus(@Payload() updateOrderDto: UpdateOrderDto) {
+    // return this.ordersService.changeOrderStatus(updateOrderDto);
+    console.log('updateOrderDto', updateOrderDto);
+    throw new NotImplementedException('Method not implemented.');
+  }
+}
